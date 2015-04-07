c81a7709ff3b2251d341251b827cfadeffd8f334
Olav Vatne <olavvatne@gmail.com>
2015-04-07 14:55:34 +0200

    Create panel method for graphics view

--- app/src/main/java/no/ntnu/tpg4850/frugalar/GraphicsView.java
+++ app/src/main/java/no/ntnu/tpg4850/frugalar/GraphicsView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.hardware.Camera;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -47,6 +48,24 @@ public class GraphicsView extends View {
             }
         }
         this.invalidate();
+    }
+
+    public void drawPanel(Canvas c, QRCode q) {
+        Point p = q.getMidpoint();
+        paint.setColor(Color.argb(150, 153,184,152));
+        //TODO: Scaling?
+        int length = 200;
+        int height = 150;
+        int padding = 10;
+        Rect panelBounds = new Rect(p.x, p.y, p.x+length, p.y+height);
+        c.drawRect(panelBounds, paint);
+
+        paint.setColor(Color.rgb(153,186,152));
+        c.drawCircle(p.x+padding, p.y+padding, 10, paint);
+        paint.setColor(Color.rgb(254,206,168));
+        //TODO: Retrieve text from valve
+        c.drawText("Updated: 13:07 07.04.15", p.x+(3*padding), p.y+padding, paint);
+        c.drawText("State: Installed 21.11.14", p.x + padding, p.y+(3*padding), paint);
     }
 }
 
